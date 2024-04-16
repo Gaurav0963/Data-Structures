e5582991bbc589c2351e17267e6d1d8bb26e3d06
GAURAV SINGH RAJPUT <gsr094@gmail.com>
2024-04-16 13:40:10 +0530

    added reverse.py

---
+++ Linked_Lists/reverse.py
@@ -0,0 +1,35 @@
+
+
+from LinkedList import LinkedList, Node
+
+class ReverseLinkedList(LinkedList):
+    def reverse(self, head: Node) -> None:
+        prev = None
+        curr = head
+
+        while curr:
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode
+        self.head = prev
+
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert(3)
+    ll.insert(4)
+    ll.insert(5)
+    ll.insert(6)
+    print(ll)
+    rll = ReverseLinkedList()
+    rll.reverse(ll.head)
+    print(rll)
+    print(dir(ReverseLinkedList))
+
+
+
+
+
+
